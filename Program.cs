fd7be4443f3f824aba1815ac6628c68ae1f3febf
Ruan <ruangustavo50711@gmail.com>
2020-11-18 16:06:32 -0300

    Exercicio 1 Finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio_de_repetição_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            do {
+                Console.Write("Digite uma nota entre 0 a 10: ");
+                nota = int.Parse(Console.ReadLine());
+
+                if(!(nota >= 0 && nota <= 10)) {
+                    Console.WriteLine("Número Inválido");
+                }
+            } while(!(nota >= 0 && nota <= 10));
+
+            Console.WriteLine("Número Aceito");
+        }
+    }
+}
